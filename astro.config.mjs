12329d412502b8a789009e184ed7224aa427065c
eduard-balamatiuc <balamatiuc2@gmail.com>
2025-04-15 17:13:23 +0300

    feat: add PR site build preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Get the base path from environment variable or use default
+// @ts-ignore
+const BASE_PATH = process.env.PUBLIC_BASE_PATH || '/';
+
 export default defineConfig({
   site: 'https://eduard-balamatiuc.com',
-  base: '/',
+  base: BASE_PATH,
 });
