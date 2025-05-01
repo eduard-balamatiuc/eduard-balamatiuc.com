45d8739a7dd743052fa89ff4f21dcf69872402ac
eduard-balamatiuc <balamatiuc2@gmail.com>
2025-05-01 18:51:37 +0300

    feat: add umami site-id for analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 // @ts-check
+import umami from "@yeskunall/astro-umami";
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://eduard-balamatiuc.com',
+  integrations: [
+    umami({
+      id: "d26387c0-ce07-49eb-bc4f-d2144349b872",}),
+  ],
 });
