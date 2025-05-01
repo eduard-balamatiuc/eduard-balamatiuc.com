2dd1cbb859bfa050424bc417474741fd77998c05
eduard-balamatiuc <balamatiuc2@gmail.com>
2025-05-01 13:10:17 +0300

    fix: update the color-theme script to consider the current user preference and store the theme in the localStorage

--- src/scripts/color-theme.js
+++ src/scripts/color-theme.js
@@ -1,6 +1,28 @@
-const themeToggle = document.querySelector('.theme-toggle');
-    if (themeToggle) {
-        themeToggle.addEventListener('click', () => {
-            document.documentElement.classList.toggle('light-theme');
-        });
+const theme = (() => {
+    const localStorageTheme = localStorage?.getItem("theme") ?? '';
+    if (['dark', 'light'].includes(localStorageTheme)) {
+        return localStorageTheme;
     }
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        return 'dark';
+    }
+    return 'light';
+})();
+
+if (theme === 'light') {
+    document.documentElement.classList.add('light-theme');
+} else {
+    document.documentElement.classList.remove('light-theme');
+}
+
+window.localStorage.setItem('theme', theme);
+
+const handleToggleClick = () => {
+    const element = document.documentElement;
+    element.classList.toggle("light-theme");
+
+    const isDark = !element.classList.contains("light-theme");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+}
+
+document.querySelector('.theme-toggle')?.addEventListener("click", handleToggleClick);
